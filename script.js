9452df5b8589b92ccb1e8fd535f24b02b763a524
greenblue467 <greenblue467@hotmail.com>
2020-02-18 17:56:50 +0800

    multiple modified

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+/*window.addEventListener("load", function() {
   $(".loading").fadeOut("slow");
   $("header").css({
     display: "block"
@@ -9,7 +9,7 @@ window.addEventListener("load", function() {
   $("footer").css({
     display: "block"
   });
-});
+});*/
 
 //global
 $(window).scroll(function() {
