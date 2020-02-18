f4fbf597a5c3d017373d230e526e6addb448a7bb
greenblue467 <greenblue467@hotmail.com>
2020-02-18 17:44:48 +0800

    modify script

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-$("window").on("load", function() {
+window.addEventListener("load", function() {
   $(".loading").fadeOut("slow");
+  $("header").css({
+    display: "block"
+  });
+  $("main").css({
+    display: "block"
+  });
+  $("footer").css({
+    display: "block"
+  });
 });
 
 //global
